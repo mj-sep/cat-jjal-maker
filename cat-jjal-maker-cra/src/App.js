a757a2e7c647841c4d60e0f8f76a9c2ff283e717
mj-sep <kayla.sep9@gmail.com>
2023-10-25 13:32:14 +0900

    [FIX] url property 변경 문제로 인한 API 접근 수정

--- cat-jjal-maker-cra/src/App.js
+++ cat-jjal-maker-cra/src/App.js
@@ -18,7 +18,8 @@ const fetchCat = async (text) => {
   const OPEN_API_DOMAIN = "https://cataas.com";
   const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
   const responseJson = await response.json();
-  return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+  console.log('${OPEN_API_DOMAIN}/cat/${responseJson._id}/says/${text}');
+  return `${OPEN_API_DOMAIN}/cat/${responseJson._id}/says/${text}`;
 };
 
 function CatItem(props) {
